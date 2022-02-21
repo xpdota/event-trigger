bee76683f5fa657b5c5af24bd30bbc970c461975
xp <smurfs@smurfs.verymad.net>
2022-02-21 07:37:24 -0800

    Added damage/heal display to ticks

--- xivsupport/src/main/java/gg/xp/xivsupport/events/actlines/events/TickEvent.java
+++ xivsupport/src/main/java/gg/xp/xivsupport/events/actlines/events/TickEvent.java
@@ -1,21 +1,29 @@
 package gg.xp.xivsupport.events.actlines.events;
 
 import gg.xp.reevent.events.BaseEvent;
+import gg.xp.xivsupport.events.actlines.events.abilityeffect.AbilityEffect;
+import gg.xp.xivsupport.events.actlines.events.abilityeffect.DamageTakenEffect;
+import gg.xp.xivsupport.events.actlines.events.abilityeffect.HealEffect;
+import gg.xp.xivsupport.events.actlines.events.abilityeffect.HitSeverity;
 import gg.xp.xivsupport.models.XivCombatant;
 
-public class TickEvent extends BaseEvent implements HasTargetEntity {
+import java.io.Serial;
+import java.util.Collections;
+import java.util.List;
 
+public class TickEvent extends BaseEvent implements HasTargetEntity, HasEffects {
 
+	@Serial
+	private static final long serialVersionUID = -78631681579667812L;
 	private final XivCombatant combatant;
 	private final TickType type;
 	private final long damage;
 	private final long rawEffectId;
 
-	public TickEvent(XivCombatant combatant, TickType type, long damage, long rawEffectId) {
-
+	public TickEvent(XivCombatant combatant, TickType type, long damageOrHeal, long rawEffectId) {
 		this.combatant = combatant;
 		this.type = type;
-		this.damage = damage;
+		this.damage = damageOrHeal;
 		this.rawEffectId = rawEffectId;
 	}
 
@@ -38,5 +46,15 @@ public class TickEvent extends BaseEvent implements HasTargetEntity {
 
 	public long getRawEffectId() {
 		return rawEffectId;
+	}
+
+	@Override
+	public List<AbilityEffect> getEffects() {
+		if (type == TickType.HOT) {
+			return Collections.singletonList(new HealEffect(HitSeverity.NORMAL, damage));
+		}
+		else {
+			return Collections.singletonList(new DamageTakenEffect(HitSeverity.NORMAL, damage));
+		}
 	}
 }
