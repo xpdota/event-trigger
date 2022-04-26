ca6734ee2770d8f11a6f114a3ddc3e2601047198
xp <smurfs@smurfs.verymad.net>
2022-04-26 11:25:01 -0700

    Pairs of marker mechanic

--- triggers/src/main/java/gg/xp/xivsupport/events/triggers/duties/ewult/Dragonsong.java
+++ triggers/src/main/java/gg/xp/xivsupport/events/triggers/duties/ewult/Dragonsong.java
@@ -10,17 +10,25 @@ import gg.xp.xivsupport.events.actlines.events.AbilityCastStart;
 import gg.xp.xivsupport.events.actlines.events.HeadMarkerEvent;
 import gg.xp.xivsupport.events.state.XivState;
 import gg.xp.xivsupport.events.triggers.seq.SequentialTrigger;
+import gg.xp.xivsupport.models.XivCombatant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @CalloutRepo("Dragonsong's Reprise")
 public class Dragonsong implements FilteredEventHandler {
 
-	private final ModifiableCallout<HeadMarkerEvent> firstCleaveMarker = new ModifiableCallout<>("Quad Marker", "Marker ({set} set)");
-	private final ModifiableCallout<AbilityCastStart> holiestOfHoly = ModifiableCallout.durationBasedCall("Holiest of Holy", "Raidwide");
-	private final ModifiableCallout<AbilityCastStart> emptyDimension = ModifiableCallout.durationBasedCall("Empty Dimension", "Donut");
-	private final ModifiableCallout<AbilityCastStart> heavensblaze = ModifiableCallout.durationBasedCall("Heavensblaze", "Stack on {event.target}");
+	private static final Logger log = LoggerFactory.getLogger(Dragonsong.class);
 
+	private final ModifiableCallout<HeadMarkerEvent> p1_firstCleaveMarker = new ModifiableCallout<>("Quad Marker", "Marker ({set} set)");
+	private final ModifiableCallout<AbilityCastStart> p1_holiestOfHoly = ModifiableCallout.durationBasedCall("Holiest of Holy", "Raidwide");
+	private final ModifiableCallout<AbilityCastStart> p1_emptyDimension = ModifiableCallout.durationBasedCall("Empty Dimension", "Donut");
+	private final ModifiableCallout<AbilityCastStart> p1_heavensblaze = ModifiableCallout.durationBasedCall("Heavensblaze", "Stack on {event.target}");
+
+	private final ModifiableCallout<HeadMarkerEvent> p1_wtfAreTheseMarkers = new ModifiableCallout<>("The Other P1 Markers", "({adjustedId}) marker with {partner}");
 
 	private final XivState state;
 
@@ -38,10 +46,12 @@ public class Dragonsong implements FilteredEventHandler {
 		int id = (int) event.getAbility().getId();
 		final ModifiableCallout<AbilityCastStart> call;
 		switch (id) {
-			case 0x62D4 -> call = holiestOfHoly;
-			case 0x62DA -> call = emptyDimension;
-			case 0x62DD -> call = heavensblaze;
-			default -> {return;}
+			case 0x62D4 -> call = p1_holiestOfHoly;
+			case 0x62DA -> call = p1_emptyDimension;
+			case 0x62DD -> call = p1_heavensblaze;
+			default -> {
+				return;
+			}
 		}
 		context.accept(call.getModified(event));
 	}
@@ -58,28 +68,49 @@ public class Dragonsong implements FilteredEventHandler {
 	@HandleEvents(order = -50_000)
 	public void sequentialHeadmarkSolver(EventContext context, HeadMarkerEvent event) {
 		getHeadmarkOffset(event);
-		// and also feed this sequential trigger
-		fourHeadMark.feed(context, event);
 	}
 
-	private final SequentialTrigger<HeadMarkerEvent> fourHeadMark = new SequentialTrigger<>(20_000, HeadMarkerEvent.class,
-			e -> getHeadmarkOffset(e) == 0,
+	@HandleEvents
+	public void feedSeq(EventContext context, BaseEvent event) {
+		p1_fourHeadMark.feed(context, event);
+		p1_pairsOfMarkers.feed(context, event);
+	}
+
+	private final SequentialTrigger<BaseEvent> p1_fourHeadMark = new SequentialTrigger<>(30_000, BaseEvent.class,
+			e -> (e instanceof AbilityCastStart acs) && acs.getAbility().getId() == 0x62DD,
 			(e1, s) -> {
-				// First marker
-				if (e1.getTarget().isThePlayer()) {
-					s.accept(firstCleaveMarker.getModified(Map.of("set", "first")));
-					return;
-				}
 				// 2-4 markers
-				for (int count = 2; count <= 4; count++) {
-					HeadMarkerEvent e = s.waitEvent(HeadMarkerEvent.class, event -> getHeadmarkOffset(event) == 0);
-					if (e.getTarget().isThePlayer()) {
-						s.accept(firstCleaveMarker.getModified(Map.of("set", "first")));
-						return;
-					}
+				String set;
+				// If you aren't one of the first 4, you're one of the second four
+				if (s.waitEvents(4, HeadMarkerEvent.class, event -> getHeadmarkOffset(event) == 0)
+						.stream().anyMatch(e -> e.getTarget().isThePlayer())) {
+					set = "first";
 				}
-				// If you weren't one of the first four, you're one of the second four
-				s.accept(firstCleaveMarker.getModified(Map.of("set", "second")));
+				else {
+					set = "second";
+				}
+				s.accept(p1_firstCleaveMarker.getModified(Map.of("set", set)));
 			});
+
+	private final SequentialTrigger<BaseEvent> p1_pairsOfMarkers = new SequentialTrigger<>(20_000, BaseEvent.class,
+			e -> e instanceof AbilityCastStart acs && acs.getAbility().getId() == 0x62D0,
+			(e1, s) -> {
+				List<HeadMarkerEvent> marks = s.waitEventsUntil(8, HeadMarkerEvent.class, e -> {
+					int headmarkOffset = getHeadmarkOffset(e);
+					return headmarkOffset >= 47 && headmarkOffset <= 50;
+				}, AbilityCastStart.class, acs -> acs.getAbility().getId() == 0x62DE);
+				marks.stream().filter(e -> e.getTarget().isThePlayer())
+						.findAny()
+						.ifPresentOrElse(myMark -> {
+							Optional<HeadMarkerEvent> partnerMarker = marks.stream().filter(e -> !e.getTarget().isThePlayer() && e.getMarkerId() == myMark.getMarkerId())
+									.findAny();
+							int adjustedId = getHeadmarkOffset(myMark);
+							XivCombatant partner = partnerMarker.map(HeadMarkerEvent::getTarget).orElse(null);
+							s.accept(p1_wtfAreTheseMarkers.getModified(Map.of(
+											"adjustedId", adjustedId,
+											"partner", partner == null ? "nobody" : partner)));
+						}, () -> log.error("No personal headmarker! Collected: [{}]", marks));
+			}
+	);
 
 }
