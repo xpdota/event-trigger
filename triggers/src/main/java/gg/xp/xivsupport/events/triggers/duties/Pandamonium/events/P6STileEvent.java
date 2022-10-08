f5523d45f00c2439e87bcf258e2d676c85d9ccae
xp <smurfs@smurfs.verymad.net>
2022-10-07 17:14:10 -0700

    p6s improvements

--- triggers/src/main/java/gg/xp/xivsupport/events/triggers/duties/Pandamonium/events/P6STileEvent.java
+++ triggers/src/main/java/gg/xp/xivsupport/events/triggers/duties/Pandamonium/events/P6STileEvent.java
@@ -2,6 +2,7 @@ package gg.xp.xivsupport.events.triggers.duties.Pandamonium.events;
 
 import gg.xp.reevent.events.BaseEvent;
 import gg.xp.xivsupport.events.actlines.events.HasPrimaryValue;
+import gg.xp.xivsupport.models.Position;
 
 public class P6STileEvent extends BaseEvent implements HasPrimaryValue {
 	private final int x;
@@ -35,5 +36,9 @@ public class P6STileEvent extends BaseEvent implements HasPrimaryValue {
 
 	public int getIndex() {
 		return index;
+	}
+
+	public Position tilePos() {
+		return Position.of2d(85 + 10 * x, 85 + 10 * y);
 	}
 }
