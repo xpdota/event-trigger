faeaa0b1e4cd344838a72a4699a3258d9fb73512
xp <smurfs@smurfs.verymad.net>
2022-12-19 22:15:24 -0800

    Relative time display option

--- xivsupport/src/main/java/gg/xp/xivsupport/gui/components/ReadOnlyText.java
+++ xivsupport/src/main/java/gg/xp/xivsupport/gui/components/ReadOnlyText.java
@@ -8,7 +8,7 @@ public class ReadOnlyText extends JTextArea {
 	// Implementation notes: In order to get this to behave the way we want to under most circumstances
 	// (i.e. stretch to fill available width), we need to:
 	// 1. Lock the preferred size
-	// 2. Report minimum size as the great of true minimum vs original preferred. Otherwise, if you expand the window
+	// 2. Report minimum size as the greater of true minimum vs original preferred. Otherwise, if you expand the window
 	// a bit horizontally, it won't fill the new space.
 	public ReadOnlyText(String text) {
 		super(text);
