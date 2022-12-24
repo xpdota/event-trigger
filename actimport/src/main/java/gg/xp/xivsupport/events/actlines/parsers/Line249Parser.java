38395be27494518c4940c2fe8c3e90f694133722
xp <smurfs@smurfs.verymad.net>
2022-12-24 12:40:02 -0800

    Set game language from log file

---
+++ actimport/src/main/java/gg/xp/xivsupport/events/actlines/parsers/Line249Parser.java
@@ -0,0 +1,42 @@
+package gg.xp.xivsupport.events.actlines.parsers;
+
+import gg.xp.reevent.events.Event;
+import gg.xp.xivsupport.events.ActImportOnly;
+import gg.xp.xivsupport.lang.GameLanguage;
+import gg.xp.xivsupport.lang.GameLanguageInfoEvent;
+import org.picocontainer.PicoContainer;
+
+import java.time.ZonedDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SuppressWarnings("unused")
+public class Line249Parser extends AbstractACTLineParser<Line249Parser.Fields> implements ActImportOnly {
+
+	public Line249Parser(PicoContainer container) {
+		super(container, 249, Fields.class);
+	}
+
+	enum Fields {
+		text
+	}
+
+	private static final Pattern langId = Pattern.compile("Selected Language ID: (\\d+), .*");
+	private static final Pattern langName = Pattern.compile("Selected Language ID: ([A-Za-z]+), .*");
+
+	@Override
+	protected Event convert(FieldMapper<Fields> fields, int lineNumber, ZonedDateTime time) {
+		String rawText = fields.getString(Fields.text);
+		Matcher langIdMatcher = langId.matcher(rawText);
+		if (langIdMatcher.matches()) {
+			GameLanguage lang = GameLanguage.values()[Integer.parseInt(langIdMatcher.group(1))];
+			return new GameLanguageInfoEvent(lang);
+		}
+		Matcher langNameMatcher = langName.matcher(rawText);
+		if (langNameMatcher.matches()) {
+			GameLanguage lang = GameLanguage.valueOf(langNameMatcher.group(1));
+			return new GameLanguageInfoEvent(lang);
+		}
+			return null;
+	}
+}
