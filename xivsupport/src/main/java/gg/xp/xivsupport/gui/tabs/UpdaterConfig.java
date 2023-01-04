19f8421ae19ada3c6b5b28dce4084ad9720498fb
xp <smurfs@smurfs.verymad.net>
2023-01-04 09:10:42 -0800

    Added addon description field and auto-refreshing of addon info

--- xivsupport/src/main/java/gg/xp/xivsupport/gui/tabs/UpdaterConfig.java
+++ xivsupport/src/main/java/gg/xp/xivsupport/gui/tabs/UpdaterConfig.java
@@ -8,19 +8,26 @@ import gg.xp.xivsupport.persistence.Platform;
 import gg.xp.xivsupport.persistence.SimplifiedPropertiesFilePersistenceProvider;
 import gg.xp.xivsupport.persistence.settings.CustomJsonListSetting;
 import gg.xp.xivsupport.persistence.settings.StringSetting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @ScanMe
 public class UpdaterConfig {
 
+	private static final Logger log = LoggerFactory.getLogger(UpdaterConfig.class);
 	private static final String propsOverrideFileName = "update.properties";
 	private static final Pattern allowedDirName = Pattern.compile("([A-Za-z][A-Za-z0-9-_]+)");
+	private static final ObjectMapper mapper = new ObjectMapper();
 	private final File propsOverride;
 	private final PersistenceProvider updatePropsFilePers;
 	private final File installDir;
@@ -28,6 +35,7 @@ public class UpdaterConfig {
 	private final StringSetting urlTemplateSetting;
 	private final CustomJsonListSetting<AddonDef> addonSetting;
 	private final StringSetting addonUrlsSetting;
+	private final ExecutorService exs = Executors.newCachedThreadPool();
 	private Runnable updateRunnable = () -> {
 
 	};
@@ -42,6 +50,7 @@ public class UpdaterConfig {
 		addonSetting = CustomJsonListSetting.<AddonDef>builder(pers, new TypeReference<>() {
 		}, "update-config.addon-manager.addon-list", "update-config.addon-manager.addon-list-failed").build();
 		addonSetting.addListener(this::syncAddonConfig);
+		exs.submit(this::refreshAddons);
 	}
 
 	public StringSetting getBranchSetting() {
@@ -64,17 +73,21 @@ public class UpdaterConfig {
 		if (!addonInfoUrl.endsWith("INFO")) {
 			addonInfoUrl += "/INFO";
 		}
-		ObjectMapper mapper = new ObjectMapper();
+		AddonDef newAddonDef = getAddonDef(addonInfoUrl);
+		addonSetting.addItem(newAddonDef);
+	}
+
+	private AddonDef getAddonDef(String url) {
 		AddonDef newAddonDef;
 		try {
-			newAddonDef = mapper.readValue(new URL(addonInfoUrl), new TypeReference<>() {
+			newAddonDef = mapper.readValue(new URL(url), new TypeReference<>() {
 			});
 		}
 		catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 		if (newAddonDef.url == null) {
-			newAddonDef.url = addonInfoUrl;
+			newAddonDef.url = url;
 		}
 		if (newAddonDef.name == null) {
 			throw new AddonValidationException("Addon did not have a name");
@@ -88,7 +101,7 @@ public class UpdaterConfig {
 		if (newAddonDef.urlPattern == null) {
 			throw new AddonValidationException("Addon did not define a download URL");
 		}
-		addonSetting.addItem(newAddonDef);
+		return newAddonDef;
 	}
 
 	// TODO: also need a way of refreshing addon info from INFO files, in case the author wants to change the icon,
@@ -99,6 +112,22 @@ public class UpdaterConfig {
 				.map(ad -> ad.dirName + ':' + ad.urlPattern)
 				.collect(Collectors.joining("\n"))
 		);
+	}
+
+	public void refreshAddons() {
+		List<AddonDef> addonsBefore = addonSetting.getItems();
+		addonsBefore.forEach(addon -> {
+			AddonDef addonDef;
+			try {
+				addonDef = getAddonDef(addon.url);
+			}
+			catch (Throwable t) {
+				log.error("Error updating info for addon", t);
+				return;
+			}
+			addon.readFrom(addonDef);
+		});
+		addonSetting.commit();
 	}
 
 
