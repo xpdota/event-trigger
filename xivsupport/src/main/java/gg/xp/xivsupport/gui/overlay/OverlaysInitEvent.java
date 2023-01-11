379614e6407a9a0c7c1b1c3d776a5582776be0f5
xp <smurfs@smurfs.verymad.net>
2023-01-10 22:28:22 -0800

    Classified OverlaysInitEvent as a system event

--- xivsupport/src/main/java/gg/xp/xivsupport/gui/overlay/OverlaysInitEvent.java
+++ xivsupport/src/main/java/gg/xp/xivsupport/gui/overlay/OverlaysInitEvent.java
@@ -1,9 +1,11 @@
 package gg.xp.xivsupport.gui.overlay;
 
 import gg.xp.reevent.events.BaseEvent;
+import gg.xp.reevent.events.SystemEvent;
 
 import java.io.Serial;
 
+@SystemEvent
 public class OverlaysInitEvent extends BaseEvent {
 	@Serial
 	private static final long serialVersionUID = 955609138557290272L;
