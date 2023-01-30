e05da319127a7dae29070c97ab67fe0dc49bfc22
XP <smurfs@smurfs.verymad.net>
2023-01-29 18:41:55 -0800

    More AMs and triggers

--- trigger-support/src/main/java/gg/xp/xivsupport/events/triggers/support/MechAssignmentEvent.java
+++ trigger-support/src/main/java/gg/xp/xivsupport/events/triggers/support/MechAssignmentEvent.java
@@ -2,18 +2,22 @@ package gg.xp.xivsupport.events.triggers.support;
 
 import gg.xp.reevent.events.BaseEvent;
 import gg.xp.xivsupport.models.XivPlayerCharacter;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.Serial;
 import java.util.Collections;
 import java.util.Map;
 
 public abstract class MechAssignmentEvent<K> extends BaseEvent {
+	@Serial
+	private static final long serialVersionUID = -5975570546647941261L;
 	private final Map<K, XivPlayerCharacter> assignments;
 
 	protected MechAssignmentEvent(Map<K, XivPlayerCharacter> assignments) {
 		this.assignments = assignments;
 	}
 
-	public K forPlayer(XivPlayerCharacter xpc) {
+	public @Nullable K forPlayer(XivPlayerCharacter xpc) {
 		return assignments.entrySet().stream()
 				.filter(e -> e.getValue().equals(xpc))
 				.map(Map.Entry::getKey)
