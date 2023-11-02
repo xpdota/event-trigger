289ff67afff6153a825a78e6bcd184438554f885
XP <smurfs@smurfs.verymad.net>
2023-11-01 17:07:33 -0700

    6.51 updates, map tab fix

--- xivdata/src/main/java/gg/xp/xivdata/data/MapLibrary.java
+++ xivdata/src/main/java/gg/xp/xivdata/data/MapLibrary.java
@@ -6,9 +6,20 @@ public class MapLibrary {
 
 	private static final MapLibraryImpl INSTANCE = new MapLibraryImpl(() -> ReadCsv.cellsFromResource("/xiv/maps/Map.csv"));
 
+	private static XivMap createUnknown(long id) {
+		return new XivMap(0, 0, 100, null, "Unknown %s".formatted(id), "Unk", "Unk");
+	}
+
 	public static XivMap forId(long id) {
 		XivMap map = INSTANCE.forId(id);
-		return (map == null) ? XivMap.UNKNOWN : map;
+		// Maps that don't exist in whatever version of the game files may still have blank stub entries, but these
+		// cause the map tab to malfunction due to having a scale factor of 0.
+		if (map == null || map.getScaleFactor() == 0.0) {
+			return createUnknown(id);
+		}
+		else {
+			return map;
+		}
 	}
 
 	public static @Nullable XivMap forFilename(String mapFileName) {
