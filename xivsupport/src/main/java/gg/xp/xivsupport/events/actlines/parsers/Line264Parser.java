53d611dc4f6b283cb4f53513cb7118527905eef3
XP <smurfs@smurfs.verymad.net>
2024-06-17 18:26:31 -0700

    Implement AnimationLockEvent

--- xivsupport/src/main/java/gg/xp/xivsupport/events/actlines/parsers/Line264Parser.java
+++ xivsupport/src/main/java/gg/xp/xivsupport/events/actlines/parsers/Line264Parser.java
@@ -3,6 +3,7 @@ package gg.xp.xivsupport.events.actlines.parsers;
 import gg.xp.reevent.events.Event;
 import gg.xp.reevent.scan.HandleEvents;
 import gg.xp.xivsupport.events.actlines.events.AbilityUsedEvent;
+import gg.xp.xivsupport.events.actlines.events.AnimationLockEvent;
 import gg.xp.xivsupport.events.actlines.events.SnapshotLocationDataEvent;
 import gg.xp.xivsupport.events.misc.OverwritingRingBuffer;
 import gg.xp.xivsupport.models.Position;
@@ -19,13 +20,13 @@ public class Line264Parser extends AbstractACTLineParser<Line264Parser.Fields> {
 	}
 
 	enum Fields {
-		entityId, abilityId, sequence, hasData, x, y, z, rotation
+		entityId, abilityId, sequence, hasData, x, y, z, rotation, animationLock
 	}
 
 	private final OverwritingRingBuffer<AbilityUsedEvent> buffer = new OverwritingRingBuffer<>(32);
 
 	@HandleEvents
-	public void consumeAbilityCast(AbilityUsedEvent aue) {
+	public void consumeAbilityUse(AbilityUsedEvent aue) {
 		if (aue.isFirstTarget()) {
 			buffer.write(aue);
 		}
@@ -33,10 +34,6 @@ public class Line264Parser extends AbstractACTLineParser<Line264Parser.Fields> {
 
 	@Override
 	protected Event convert(FieldMapper<Fields> fields, int lineNumber, ZonedDateTime time) {
-		int hasData = fields.getInt(Fields.hasData);
-		if (hasData == 0) {
-			return null;
-		}
 		long entity = fields.getHex(Fields.entityId);
 		long ability = fields.getHex(Fields.abilityId);
 		long sequenceId = fields.getHex(Fields.sequence);
@@ -45,24 +42,44 @@ public class Line264Parser extends AbstractACTLineParser<Line264Parser.Fields> {
 			if (last.getSource().getId() == entity
 			    && last.getAbility().getId() == ability
 			    && last.getSequenceId() == sequenceId) {
-				double x = fields.getDouble(Fields.x);
-				double y = fields.getDouble(Fields.y);
-				double z = fields.getDouble(Fields.z);
-				double h = fields.getDouble(Fields.rotation);
-				SnapshotLocationDataEvent out;
-				if (x == 0.0 && y == 0.0 && z == 0.0) {
-					if (h == 0.0) {
-						return null;
+
+				MultipleEvent out = new MultipleEvent();
+
+				// Cast location/angle
+				// First, check if the line indicates that such data is actually present.
+				int hasData = fields.getInt(Fields.hasData);
+				SnapshotLocationDataEvent slde;
+				// 0 = no data, 1 = data, 256 = error
+				if (hasData == 1) {
+					double x = fields.getDouble(Fields.x);
+					double y = fields.getDouble(Fields.y);
+					double z = fields.getDouble(Fields.z);
+					double h = fields.getDouble(Fields.rotation);
+					if (x == 0.0 && y == 0.0 && z == 0.0) {
+						if (h == 0.0) {
+							slde = null;
+						}
+						else {
+							slde = new SnapshotLocationDataEvent(last, h);
+						}
 					}
 					else {
-						out = new SnapshotLocationDataEvent(last, h);
+						Position pos = new Position(x, y, z, h);
+						slde = new SnapshotLocationDataEvent(last, pos);
+					}
+					if (slde != null) {
+						last.setLocationInfo(slde);
+						out.add(slde);
 					}
 				}
-				else {
-					Position pos = new Position(x, y, z, h);
-					out = new SnapshotLocationDataEvent(last, pos);
+				if (fields.hasField(Fields.animationLock)) {
+					double animLock = fields.getDouble(Fields.animationLock);
+					if (animLock > 0) {
+						AnimationLockEvent lock = new AnimationLockEvent(last, animLock);
+						last.setAnimationLock(lock.getInitialDuration());
+						out.add(lock);
+					}
 				}
-				last.setLocationInfo(out);
 				return out;
 			}
 		}
