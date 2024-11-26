af25041714224f95ac50297a782308782d559bcb
XP <smurfs@smurfs.verymad.net>
2024-11-26 06:41:01 -0800

    Fix missing icons, more FRU stub

--- triggers/triggers-dt/src/main/java/gg/xp/xivsupport/triggers/ultimate/FRU.java
+++ triggers/triggers-dt/src/main/java/gg/xp/xivsupport/triggers/ultimate/FRU.java
@@ -1,10 +1,15 @@
 package gg.xp.xivsupport.triggers.ultimate;
 
+import gg.xp.reevent.events.BaseEvent;
 import gg.xp.reevent.events.EventContext;
 import gg.xp.reevent.scan.AutoChildEventHandler;
 import gg.xp.reevent.scan.FilteredEventHandler;
 import gg.xp.xivdata.data.duties.*;
+import gg.xp.xivsupport.events.actlines.events.HasSourceEntity;
+import gg.xp.xivsupport.events.actlines.events.HasTargetEntity;
 import gg.xp.xivsupport.events.state.XivState;
+import gg.xp.xivsupport.models.XivCombatant;
+import gg.xp.xivsupport.models.XivPlayerCharacter;
 
 public class FRU extends AutoChildEventHandler implements FilteredEventHandler {
 	private final XivState state;
@@ -25,11 +30,55 @@ public class FRU extends AutoChildEventHandler implements FilteredEventHandler {
 	 * 1. Baits on player
 	 * 2. Hits where #1 set was (i.e. dodge)
 	 * 3. Move back
-	 * 4. ?
+	 * 4. Move back
 	 * Spread if lightning, stack if fire
+	 * Tankbuster, leaves debuff (2451 / 0x993?)
 	 *
 	 * Ilusion mechanic
 	 * Tank thing at the start
-	 * Stack/spread based on last
+	 * Stack/spread based on last (spread like DSR)
+	 *
+	 * Check clone for safe spot
+	 * Do proteans again
+	 * Has stacks/spread mech
+	 * Two tethers
+	 *
+	 * Four tether mechanic
 	 */
+
+	public enum MechType {
+		Fire,
+		Lightning,
+		Holy
+	}
+
+	public MechType getLastMechType() {
+		return null;
+	}
+
+	public static class FruP1TetherEvent extends BaseEvent implements HasSourceEntity, HasTargetEntity {
+		private final MechType mechType;
+		private final XivCombatant source;
+		private final XivPlayerCharacter target;
+
+		public FruP1TetherEvent(MechType mechType, XivCombatant source, XivPlayerCharacter target) {
+			this.mechType = mechType;
+			this.source = source;
+			this.target = target;
+		}
+
+		public MechType getMechType() {
+			return mechType;
+		}
+
+		@Override
+		public XivCombatant getSource() {
+			return source;
+		}
+
+		@Override
+		public XivPlayerCharacter getTarget() {
+			return target;
+		}
+	}
 }
