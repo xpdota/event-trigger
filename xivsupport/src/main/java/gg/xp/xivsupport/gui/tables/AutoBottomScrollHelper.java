e43cb1ff55ac97ad4a63b3a8c135b24bd72eca73
XP <smurfs@smurfs.verymad.net>
2026-03-11 16:13:27 -0700

    Bottom scroll improvements

--- xivsupport/src/main/java/gg/xp/xivsupport/gui/tables/AutoBottomScrollHelper.java
+++ xivsupport/src/main/java/gg/xp/xivsupport/gui/tables/AutoBottomScrollHelper.java
@@ -5,14 +5,29 @@ import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.function.Consumer;
 
 public class AutoBottomScrollHelper extends JScrollPane {
 
 	private static final Logger log = LoggerFactory.getLogger(AutoBottomScrollHelper.class);
 	private static final boolean oldBehavior = "true".equals(System.getProperty("bottom-scroll.old-behavior"));
+	/**
+	 * Callback for when auto bottom scroll is currently active or not. Wraps the user-supplied stateCallback to
+	 * reset the value of autoScrollEnabled back to what it was before the callback. Thus, the callback is not allowed
+	 * to modify the state of autoScrollEnabled.
+	 */
 	private final Consumer<Boolean> stateCallback;
+	/**
+	 * Whether auto scroll is enabled or not. Note that in order for auto-scrolling to actually be **active**,
+	 * {@link #atBottom} must also be true. For example, if you auto-scroll is active, but you scroll up, then auto
+	 * scrolling will be interrupted (atBottom = false), but this will remain true.
+	 */
 	private boolean autoScrollEnabled;
+	/**
+	 * Whether we are currently (or *should* be) at the bottom of the scrollable area.
+	 */
 	private boolean atBottom;
 	private volatile int oldMax;
 	private volatile int oldValue;
@@ -32,6 +47,19 @@ public class AutoBottomScrollHelper extends JScrollPane {
 			stateCallback.accept(val);
 			autoScrollEnabled = old;
 		};
+		// Workaround to allow you to re-activate bottom scrolling by mouse wheeling down when the scrollable area
+		// is already all the way at the bottom.
+		addMouseWheelListener(e -> {
+			if (e.getWheelRotation() > 0 && autoScrollEnabled && !atBottom) {
+				JScrollBar sb = getVerticalScrollBar();
+				BoundedRangeModel brm = sb.getModel();
+				// Is the scrollbar already at the bottom?
+				if (brm.getValue() + brm.getExtent() == brm.getMaximum()) {
+					atBottom = true;
+					stateCallback.accept(true);
+				}
+			}
+		});
 
 	}
 
@@ -75,12 +103,16 @@ public class AutoBottomScrollHelper extends JScrollPane {
 		return new JScrollPane.ScrollBar(JScrollBar.VERTICAL) {
 			@Override
 			public void setValues(int newValue, int newExtent, int newMin, int newMax) {
-//				log.info("Value: {} -> {} ; Max: {} -> {}", oldValue, newValue, oldMax, newMax);
-				// This branch means more data has been added to the table
+				log.info("Value: {} -> {} ; Max: {} -> {}", oldValue, newValue, oldMax, newMax);
+				// This branch means more data has been added to the table.
 				if (newMax != oldMax || newExtent != oldExtent) {
 					if (atBottom && autoScrollEnabled) {
+						// Do the auto scroll
 						newValue = newMax - newExtent;
 					}
+				}
+				// Not an actual scroll - just quirks of how the scrolling works
+				else if (newValue == oldValue && newMax == oldMax) {
 				}
 				// This branch means that the user has scrolled
 				else {
@@ -129,17 +161,17 @@ public class AutoBottomScrollHelper extends JScrollPane {
 	}
 
 	private void doScrollIfEnabled() {
-		log.trace("doScrollIfEnabled");
+//		log.trace("doScrollIfEnabled");
 		if (autoScrollEnabled) {
 			doScroll();
 		}
 	}
 
 	private void doScroll() {
-		log.trace("doScroll");
+//		log.trace("doScroll");
 		JScrollBar scrollBar = getVerticalScrollBar();
-		int max = scrollBar.getMaximum();
-		log.trace("Max: {}", max);
+//		int max = scrollBar.getMaximum();
+//		log.trace("Max: {}", max);
 		scrollBar.setValue(Integer.MAX_VALUE);
 		atBottom = true;
 	}
